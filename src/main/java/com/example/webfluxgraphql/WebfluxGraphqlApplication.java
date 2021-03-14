63587d54150bff134bb6a5299185f1f10324aa4d
Dinesh Venkatasubramanian <dvenkatasubraman@paypal.com>
2021-03-14 11:26:04 +0530

    Initial commit

---
+++ src/main/java/com/example/webfluxgraphql/WebfluxGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webfluxgraphql/WebfluxGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
